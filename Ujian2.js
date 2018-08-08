ae560924d899ed9dee1924b9530181ae94946c48
Rsteven <rsteven18pur@gmail.com>
2018-08-08 12:08:45 +0700

    Soal 2

--- Ujian2.js
+++ Ujian2.js
@@ -1,9 +1,15 @@
-var input 'kaKJB812&##@&12asj';
+var input = 'kaKJB812&##@&12asj';
+
 
 var regExhuruf = /[A-Za-z]/g;
-
 huruf = input.replace(regExhuruf, '')
-
 hasilhuruf = huruf.split('');
 
 console.log(hasilhuruf);
+
+
+var regExangka = /[^0-9]/g;
+angka = input.replace(regExangka, '');
+hasilangka = angka.split('');
+
+console.log(hasilangka);
